f2f631c90ddb0387c687cddb661ab71dc15ad2f8
michael <michael1994822@gmail.com>
2017-04-01 20:45:48 +0100

    python test2

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import codecs
+import random
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('+YrlgJ1c5YOs8NGIOUOPN2Z/Ya6zmtW2mlrynoKWm64OuqFFYIJ6Gy90AwyTZmg9bTPWUAa8bIA+tJfOgw1ekKR3/RUukTJw+9ppv08wBIF83Hx2FRqaKdcyZcUx2viZe8DXDc6l5ftaAyUNSt7cQQdB04t89/1O/w1cDnyilFU=') #Your Channel Access Token
@@ -30,6 +31,28 @@ ans2=u'相信我，你可以的！我建議你可以先看看【'+'TED'+u'】國
 ques3=u'或許一了百了比較輕鬆'
 ans3=u'你先可以撥打衛生福利部'+'24'+u'小時安心專線：'+'0800-788-995'+u'、國際生命線協會'+'24'+u'小時電話協談：'+'1995'+u'聊聊看！'
 ans4=u'洗洗睡比較快'
+
+happyreply1=u'太棒了！我真是為你開心！'
+happyreply2=u'好事可以多跟人分享，快樂會加倍喔！'
+happyreply3=u'世界上果然有很多美好的事。'
+happyreply4=u'你開心，我窩心。'
+happyreply5=u'用心感受每天的快樂。'
+happyreply6=u'願你每天笑口常開。'
+happyreply7=u'守住你頭頂那片快樂的藍天。'
+happyreply8=u'熱愛生命，快樂無所不在！'
+happyreply9=u'讓我們一起來唱「快樂崇拜」！'
+happyreply10=u'推薦你跟我一起聽首「一百件快樂的事」。'
+
+sadreply1=u'你有的不愉快，讓我來分擔。'
+sadreply2=u'每個人都有屬於自己快樂的公式，只是等待你發掘。'
+sadreply3=u'盡力就好！'
+sadreply4=u'睡個覺起來，明天又是新的一天！'
+sadreply5=u'讓我們先一起唱首「煎熬」發洩一下！'
+sadreply6=u'一秒前的事，就當它過去了吧。'
+sadreply7=u'盡量跟我發洩吧！說完心情會好很多。'
+sadreply8=u'多跟我說說，我是你永遠的垃圾桶。'
+sadreply9=u'不要排斥不快樂，是它讓你了解快樂的珍貴。'
+sadreply10=u'現在出門去運動吧！會讓你心情變好。'
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -58,9 +81,50 @@ def handle_text_message(event):
     elif text ==ques3:
         text=ans3
     else:
-        text=ans4
-    #textlength=len(text)
-    #texttype=type(text)
+        condition=0
+        num=random.randint(1,10)
+        if condition==1        #happyreply
+            if num==1:
+                text=happyreply1
+            elif num==2:
+                text=happyreply2
+            elif num==3:
+                text=happyreply3
+            elif num==4:
+                text=happyreply4
+            elif num==5:
+                text=happyreply5
+            elif num==6:
+                text=happyreply6
+            elif num==7:
+                text=happyreply7
+            elif num==8:
+                text=happyreply8
+            elif num==9:
+                text=happyreply9
+            else:
+                text=happyreply10
+        else:                   #sadreply
+            if num==1:
+                text=sadreply1
+            elif num==2:
+                text=sadreply2
+            elif num==3:
+                text=sadreply3
+            elif num==4:
+                text=sadreply4
+            elif num==5:
+                text=sadreply5
+            elif num==6:
+                text=sadreply6
+            elif num==7:
+                text=sadreply7
+            elif num==8:
+                text=sadreply8
+            elif num==9:
+                text=sadreply9
+            else:
+                text=sadreply10
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text)  ) #reply the same message from user
